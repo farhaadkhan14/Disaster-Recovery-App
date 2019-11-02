eb337aa90ae96797fff2970b283914853e639725
Jacob Medel <jmedel@uh.edu>
2019-11-02 17:09:41 -0500

    Added buttons for info, person, and location

---
+++ App/components/DisplayPersonButton.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+
+export default class DisplayPersonButton extends Component {
+  constructor(props){
+    super(props);
+  }
+  
+  render() {
+    return (
+      <Card style={{ height: 100 }}>
+        <CardItem>
+          <Body>
+            <Text>
+              {this.props.text}
+            </Text>
+          </Body>
+        </CardItem>
+      </Card>
+    );
+  }
+}
