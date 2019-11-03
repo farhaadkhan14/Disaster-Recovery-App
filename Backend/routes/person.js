ecf4eb855a006e085c0c2b6f6de6998a29e16679
cod1r <jasonho353@gmail.com>
2019-11-02 23:14:00 -0500

    yourmom

--- Backend/routes/person.js
+++ Backend/routes/person.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator');
 const {mongoDBuri} = require('../config.json')
+const { ObjectID } = require('mongodb');
 
 const { establishConnection } = require('../database')
 const { retrieve, update, insert } = require('../dbutils')
@@ -21,12 +22,32 @@ router.post('/update',[
     return res.status(422).json({ errors: errors.array() });
   }
   console.log(req)
-  update(connection,'person',req.query.id,{
-    zipcode: req.query.zipcode,
-    lat: req.query.lat,
-    lon: req.query.lon,
-    status: req.query.status
+  update(connection,'person',req.body.id,{
+    zipcode: req.body.zipcode,
+    lat: req.body.lat,
+    lon: req.body.lon,
+    status: req.body.status
   }).then((data) => res.send(data))
+
+})
+
+router.post('/insert',[], function (req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  console.log(req)
+  insert(connection,'person',req.body).then((data) => res.send(data))
+
+})
+
+router.post('/retrieve',[], function (req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  console.log(req)
+  retrieve(connection,'person',{_id: ObjectID(req.body.id)}).then((data) => res.send(data))
 
 })
 
