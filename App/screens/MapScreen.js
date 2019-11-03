879dc01ed4302276ea43fec25e319277ff9289df
rex100h <houtanfaridi@gmail.com>
2019-11-02 22:16:30 -0500

    map stuff

--- App/screens/MapScreen.js
+++ App/screens/MapScreen.js
@@ -1,112 +1,83 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, { useState, useEvent } from 'react';
+import { MapView } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+const Marker = MapView.Marker
+const HeatMap = MapView.HeatMap
+const Circle = MapView.Circle
 
-export default function HomeScreen() {
+const Map = function({ safeLocations, badLocations, inDanger, self }) {
+  const [safe, setSafe] = useState(safeLocations)
+  const [bad, setBad] = useState(badLocations)
+  const [people, setPeople] = useState(inDanger)
+  const [position, setPosition] = useState(self)
+
+  useEvent(() => {
+    if(safeLocations != safe)
+      setSafe(safeLocations)
+    if(badLocations != bad)
+      setBad(badLocations)
+    if(inDanger != people)
+      setPeople(inDanger)
+    if(self != position)
+      setPosition(self)
+  },[safeLocations, badLocations, inDanger, self])
+
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+    <MapView
+      style={styles.container}
+      region={position}
+      showsUserLocation
+    >
+      { renderPeople(people) }
+      { renderDanger(bad) }
+      { renderSafe(safe) }
+    </MapView>
+  )
+};
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
-  );
+const renderPeople = function(people) {
+  return people.map( ({ name, description, status, latitude, longitude }) =>
+    <Marker
+      title={name}
+      description={description}
+      coordinate={{latitude, longitude}}
+      pinColor={statusColor(status)}
+    />  
+  )
 }
+
+
+const statusColor = function(status) {
+  switch(status) {
+    case 0: return '#909399'
+    case 1: return '#2ca629'
+    case 2: return '#c9c012'
+    case 3: return '#ff0000'
+  }
+}
+
+
+const renderSafe = function(areas) {
+  return areas.map( coords => 
+    <Circle 
+      center={coords}
+      radius={10}
+      fillColor={'#3c9dde'}
+    /> 
+   )
+}
+
+
+const renderDanger = function(areas) {
+  return <HeatMap points={areas} />
+}
+
 
 HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -196,3 +167,6 @@ const styles = StyleSheet.create({
     color: '#2e78b7',
   },
 });
+
+
+export default Map;
