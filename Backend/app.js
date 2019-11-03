5ad86cc4684dda39f1bb0b582f1d67ac1a1bccb8
Shashank <shashankpallerla@gmail.com>
2019-11-03 10:17:49 -0600

    added changes

--- Backend/app.js
+++ Backend/app.js
@@ -5,9 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
 
-var {router:locationRouter} = require('./routes/location');
-var {router:personRouter} = require('./routes/person');
-
 var app = express();
 
 // view engine setup
@@ -15,12 +12,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var {router:locationRouter} = require('./routes/location');
+var {router:personRouter} = require('./routes/person');
 
 app.get('/', (req, res) => res.send('Disaster Recovery App is on Azure!'))
 app.use('/location', locationRouter);
